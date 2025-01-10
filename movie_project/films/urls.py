80f362374fc4b36ba026d14d03bb227d4aa15d1f
Sophiya-ai <sophiyabr@gmail.com>
2025-01-10 16:46:51 +0200

    corrected footer display

--- movie_project/films/urls.py
+++ movie_project/films/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
+# Для работы со статическим файлами подключаем:
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.add_films, name='addFilm'),
     path('watchDBfilms/', views.display_filmsdescription, name='displayFilm'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Для работы со статическим файлами, прописываем их подключение и использование
